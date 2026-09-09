81cdb8a9941ed01df4cb8810718667cb2f624894
cl0udsw1tch <nurein.umeya@mail.utoronto.ca>
2026-09-09 01:50:46 -0400

    removed redundant var set in vite conf

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vitest/config';
 
-
-const POSTGRES_DB = "tc_test"
-
 export default defineConfig({
     test: {
         globalSetup: './tests/global-setup.ts',
         environment: 'node',
         env: {
-            POSTGRES_DB: POSTGRES_DB,
             DATABASE_URL: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@postgres:${process.env.POSTGRES_PORT}/${POSTGRES_DB}`,
         },
     },
